3b57b08d31445bf15909b175e7e2fd1a45473808
ABDURRAHMAN <123483885+48k483x@users.noreply.github.com>
2024-03-07 10:51:18 +0000

    🔬📝 Continuing experiments with Philosophers! 🍽️🤔

---
+++ philo/philo.h
@@ -0,0 +1,13 @@
+#ifndef PHILO_H
+#define PHILO_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+#include <time.h>
+#include <unistd.h>
+#include <pthread.h>
+
+
+#endif // PHILOS_H
