1a375fcf06a4ebe58ac943c4888d3203becdeaf8
ABDURRAHMAN <123483885+48k483x@users.noreply.github.com>
2024-03-09 00:11:59 +0000

    🔬📝 Continuing experiments with Philosophers! 🍽️🤔

--- philo/philo.c
+++ philo/philo.c
@@ -1,6 +1,14 @@
 #include "philo.h"
 
-int main(int ac, char **av)
+void  exit_with_message(char *message)
 {
-    
+  printf("%s\n", message);
+  return ;
+}
+
+int main(int ac, char *av[])
+{
+  (void)av;
+  if (ac < 5 || ac > 6)
+    exit_with_message("Usage: ./philo [args]");
 }
