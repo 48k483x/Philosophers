4abd6c7849366e06b3e37058cb2d4186d9bb7e84
FROST-48K483X <romaissaeabde@gmail.com>
2024-04-06 07:37:27 -0400

    everything is going well, still monitoring dead philos

---
+++ old_commit/init.c
@@ -0,0 +1,86 @@
+#include "philo.h"
+
+bool init_args(t_args *args, char **av, int ac)
+{
+    if (ac < 5 || ac > 6)
+        return (exit_error("Usage: ./philo [args]"));
+    if (!is_digit(av))
+        return (exit_error("Arguments must be digits"));
+    args->philos_num = _atoi(av[1]);
+    if (args->philos_num > PHILOS_MAX)
+        return (exit_error("Too many philosophers"));
+    if (args->philos_num < 1)
+        return (exit_error("Not enough philosophers"));
+    args->time_to_die = _atoi(av[2]);
+    args->time_to_eat = _atoi(av[3]);
+    args->time_to_sleep = _atoi(av[4]);
+    if (av[5])
+        args->philos_eat_times = _atoi(av[5]);
+    else
+        args->philos_eat_times = -1;
+    return (true);
+}
+
+void init_forks(pthread_mutex_t *forks, t_philo *philos)
+{
+    int i;
+
+    i = 0;
+    while (i < philos->philos_num)
+        pthread_mutex_init(&forks[i++], NULL);
+}
+
+void init_prog(t_philo *philos, t_prog *prog)
+{
+    prog->death_flag = 0;
+    prog->philo = philos;
+    pthread_mutex_init(&prog->dead_lock, NULL);
+    pthread_mutex_init(&prog->meal_lock, NULL);
+    pthread_mutex_init(&prog->write_lock, NULL);
+}
+
+bool init_philos(t_args *args, t_philo *philos,t_prog *prog, pthread_mutex_t *forks)
+{
+    int i;
+
+    i = 0;
+    while (i < args->philos_num)
+    {
+        philos[i].id = i + 1;
+        philos[i].eating = 0;
+        philos[i].meals_eaten = 0;
+        philos[i].last_meal = 0;
+        philos[i].phil_death_time = args->time_to_die;
+        philos[i].phil_eat_time = args->time_to_eat;
+        philos[i].phil_sleep_time = args->time_to_sleep;
+        philos[i].start_time = get_time();
+        philos[i].last_meal = get_time();
+        philos[i].eat_times = args->philos_eat_times;
+        philos[i].philos_num = args->philos_num;
+        philos[i].write_lock = &prog->write_lock;
+        philos[i].dead_lock = &prog->dead_lock;
+        philos[i].meal_lock = &prog->meal_lock;
+        philos[i].dead = 0;
+        philos[i].l_fork = &forks[i];
+        if (i == 0)
+            philos[i].r_fork = &forks[args->philos_num - 1];
+        else
+            philos[i].r_fork = &forks[i - 1];
+        if (pthread_create(&philos[i].thread, NULL, (void *)philo_life, &philos[i]) != 0)
+            return (exit_error("Error creating thread"));
+        i++;
+    }
+    i = -1;
+	while (++i < args->philos_num)
+		pthread_join(philos[i].thread, NULL);
+    return (true);
+}
+
+void detach_all(t_philo *philos)
+{
+	int i;
+
+	i = 0;
+	while (i < philos->philos_num)
+		pthread_detach(philos[i++].thread);
+}
---
+++ old_commit/philo.h
@@ -0,0 +1,86 @@
+# ifndef PHILO_H
+#define PHILO_H
+
+# include <pthread.h>
+# include <semaphore.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <unistd.h>
+# include <string.h>
+# include <time.h>
+# include <errno.h>
+# include <sys/types.h>
+# include <sys/stat.h>
+# include <fcntl.h>
+# include <sys/wait.h>
+# include <sys/mman.h>
+# include <signal.h>
+# include <stdbool.h>
+# include <sys/time.h>
+
+#define PHILOS_MAX 300
+
+// struct of philosophers
+typedef struct s_philo
+{
+    pthread_t thread; 
+    int id;
+    int eating;
+    int meals_eaten;
+    size_t last_meal;
+    size_t phil_death_time;
+    size_t phil_eat_time;
+    size_t phil_sleep_time;
+    size_t start_time;
+    int eat_times;
+    int philos_num;
+    int dead;
+    pthread_mutex_t *r_fork;
+    pthread_mutex_t *l_fork;
+    pthread_mutex_t *write_lock;
+    pthread_mutex_t *dead_lock;
+    pthread_mutex_t *meal_lock;
+} t_philo;
+
+typedef struct s_prog
+{
+    int death_flag;
+    t_philo *philo;
+    pthread_mutex_t dead_lock;
+    pthread_mutex_t meal_lock;
+    pthread_mutex_t write_lock;
+} t_prog;
+
+typedef struct s_args
+{
+    int philos_num;
+    int time_to_die;
+    int time_to_eat;
+    int time_to_sleep;
+    int philos_eat_times;
+} t_args;
+
+void print_args(t_args args);
+void print_philo(t_philo philo);
+// utils.c functions
+int _atoi(char *s);
+int _isdigit(char c);
+bool exit_error(char *s);
+bool is_digit(char **av);
+long get_time(void);
+void print(t_philo *philo, char *s);
+
+// init functions
+bool init_args(t_args *args, char **av, int ac);
+bool init_philos(t_args *args, t_philo *philos,t_prog *prog, pthread_mutex_t *forks);
+void init_forks(pthread_mutex_t *forks, t_philo *philos);
+void detach_all(t_philo *philos);
+
+// routine functions
+bool is_dead(t_philo *philo);
+void eating(t_philo *philo);
+void sleeping(t_philo *philo);
+void thinking(t_philo *philo);
+void *philo_life(void *philo);
+
+#endif // PHILO_H
